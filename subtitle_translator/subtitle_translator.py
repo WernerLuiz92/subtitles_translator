56f89758cdeb0eacbffcf5504c0c2db7124a7d40
Werner Luiz Gottschalt <wgottschalt.projects@gmail.com>
2023-12-27 17:42:12 -0300

    feat: Add functionallity to translate the .srt subtitle

---
+++ subtitle_translator/subtitle_translator.py
@@ -0,0 +1,25 @@
+# A simple CLI tool to translate subtitles
+import os
+import re
+import subprocess
+
+class TranslatorHandler:
+    @classmethod
+    def main(cls, media_file, subtitle_file, directory = "./media/"):
+        print("\n\nStarting translation process...\n\n")
+        print("Go fuck yourself and translate your subtitles in google translate!")
+        
+        return
+        
+
+    @classmethod
+    def listMediaFiles(cls):
+        pass
+
+    @classmethod
+    def listSubtitleChannels(cls, file):
+        pass
+    
+    @classmethod
+    def extractSubtitles(cls, file, channel):
+        pass
